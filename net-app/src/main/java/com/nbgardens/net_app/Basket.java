f844537738c93c1cbf498396a4b150ffa35ec61f
Jared <jaredpepper2@gmail.com>
2016-10-18 17:26:30 +0100

    Added entity to Basket

--- net-app/src/main/java/com/nbgardens/net_app/Basket.java
+++ net-app/src/main/java/com/nbgardens/net_app/Basket.java
@@ -3,14 +3,39 @@ package com.nbgardens.net_app;
 import java.util.Currency;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Table (name = "Basket")
 public class Basket {
+	@Id //ID is for PK
+	@Column (name = "Basket_ID")  
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer basketID;
+	
+	
+	@OneToOne //For FK.
+	@JoinColumn(name="User_ID", nullable = false)
+	@NotNull
 	private Integer userID;
+	
+	
+	@Column (name = "Total_Price")
 	private Currency totalPrice;
+	
 	private List<BasketProduct> basketProducts;
 	
 	
-	
+
 	//GETTERS AND SETTERS
 	public Integer getBasketID() {
 		return basketID;
