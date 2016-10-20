4aee6f7540d0b0338e6d0445ef432a52efdc1fce
Jared <jaredpepper2@gmail.com>
2016-10-20 16:52:36 +0100

    Quick Change

--- net-app/src/main/java/com/nbgardens/net_app/App.java
+++ net-app/src/main/java/com/nbgardens/net_app/App.java
@@ -8,7 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Goodbye World!" );
+        System.out.println( "A gnome walks into a bar, walks up to the bartender, and says, 'Give me a shot of your finest whiskey, and leave the bottle.\n'"
+        		+ " The bartender reaches for a bottle and says, 'Okay, but this is really good stuff. The whole bottle will cost you $150'.\n "
+        		+ "The gnome looks startled, checks his wallet, and says, 'Oh, well then I guess I'll just have the one shot. I'm a little short.'" );
       
     }
 }
