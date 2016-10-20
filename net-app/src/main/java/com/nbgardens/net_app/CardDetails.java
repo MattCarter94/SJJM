afeacce9d5db4304c4c7a1d154b219c7d6850d7c
stefan-jj <stefanps2@hotmail.com>
2016-10-20 09:32:50 +0100

    Constructors for Products and CardDetails

--- net-app/src/main/java/com/nbgardens/net_app/CardDetails.java
+++ net-app/src/main/java/com/nbgardens/net_app/CardDetails.java
@@ -35,6 +35,13 @@ public class CardDetails {
 	private Date cardExpiry;
 	
 	
+	//Constructor
+	public CardDetails(){};
+	public CardDetails(User userID, String cardNo, Date cardExpiry){
+		this.userID = userID;
+		this.cardNo = cardNo;
+		this.cardExpiry = cardExpiry;
+	};
 	
 	
 	
