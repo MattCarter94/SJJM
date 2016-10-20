e2cc6c0c6fdb818cb99f4eb1537c88bbf7311bb1
joe trost <joseph.trost@qa.com>
2016-10-20 16:35:32 +0000

    added CustomerOrderManagerOffline

---
+++ net-app/src/main/java/com/nbgardens/net_app/managers/offline/CustomerOrderManagerOffline.java
@@ -0,0 +1,33 @@
+package com.nbgardens.net_app.managers.offline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.nbgardens.net_app.entities.CustomerOrder;
+
+public class CustomerOrderManagerOffline {
+	public void persistCustomerOrder(CustomerOrder c){
+		
+	}
+	
+	public void persistCustomerOrders(List<CustomerOrder> c) {
+		
+	}
+	
+	public CustomerOrder findById(CustomerOrder co) {
+		return co;
+	}
+	
+	public ArrayList<CustomerOrder> getCustomerOrders() {
+		return new ArrayList<CustomerOrder>();
+	}
+	
+	public void updateCustomerOrder(CustomerOrder s) {
+		
+	}
+	
+	public void removeCustomerOrder(CustomerOrder c) {
+		
+	}
+	
+}
