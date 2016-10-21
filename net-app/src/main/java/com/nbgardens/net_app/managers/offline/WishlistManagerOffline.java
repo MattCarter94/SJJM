7dfa90b40f1f42cdc38535e0a13a0f222ffdd9d0
joe trost <joseph.trost@qa.com>
2016-10-21 09:26:38 +0000

    added methods to WishlistManagerOffline

--- net-app/src/main/java/com/nbgardens/net_app/managers/offline/WishlistManagerOffline.java
+++ net-app/src/main/java/com/nbgardens/net_app/managers/offline/WishlistManagerOffline.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import com.nbgardens.net_app.entities.User;
 import com.nbgardens.net_app.entities.Wishlist;
 import com.nbgardens.net_app.managers.WishlistManager;
 import com.nbgardens.net_app.util.InitialData;
@@ -15,11 +16,28 @@ public class WishlistManagerOffline implements WishlistManager {
 	
 	@Override
 	public void persistWishlist(Wishlist w){
+		List<User> u = initialData.getUsers();
+		for (int i = 0; i < u.size(); i++)
+		{
+			List<Wishlist> wish = new ArrayList<Wishlist>();
+			wish.add(u.get(i).getWishlist());
+			//generates list of wishlists, does nothing with it
+		}
+		
 		//initialData.addToWishlist(w);
 	}
 	
 	@Override
 	public void updateWishlist(Wishlist w){
-		
+		List <User> u = initialData.getUsers();
+		for (int i = 0; i < u.size(); i++)
+		{
+			if (w.getWishlistID() == u.get(i).getWishlist().getWishlistID()) {
+				//Wishlist matched
+				u.get(i).setWishlist(w);
+				//TODO add this line but needs method in initialdata
+				//initialData.setUsers(u);
+			}
+		}
 	}
 }
