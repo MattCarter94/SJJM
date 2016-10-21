e821078086de3c227723873e34b98271e74a5ccc
stefan-jj <stefanps2@hotmail.com>
2016-10-21 09:43:29 +0100

    User offline manager methods

--- net-app/src/main/java/com/nbgardens/net_app/managers/UserManager.java
+++ net-app/src/main/java/com/nbgardens/net_app/managers/UserManager.java
@@ -2,14 +2,17 @@ package com.nbgardens.net_app.managers;
 
 import java.util.List;
 
+import com.nbgardens.net_app.entities.Basket;
+import com.nbgardens.net_app.entities.CardDetails;
 import com.nbgardens.net_app.entities.User;
+import com.nbgardens.net_app.entities.Wishlist;
 
 public interface UserManager {
-	public void persistUser(User u);
-	public void persistUsers(List<User> u);
+	public void persistUser(CardDetails card, Wishlist wishlist, Basket basket, String name, String surname, String email, String phoneNo, String password, Integer permissions, String add1, String add2, String town, String county, String postcode);
 	public User getUser(Integer id);
 	public List<User> getUsers();
 	
-	public void updateUser(User u);
-	public void removeUser(User u);
+	public void updateUserEmail(Integer id, String email);
+	public void updateUserPhoneNo(Integer id, String phoneNo);
+	public void updateUserPassword(Integer id, String password);
 }
--- net-app/src/main/java/com/nbgardens/net_app/managers/offline/UserManagerOffline.java
+++ net-app/src/main/java/com/nbgardens/net_app/managers/offline/UserManagerOffline.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import com.nbgardens.net_app.entities.Basket;
+import com.nbgardens.net_app.entities.CardDetails;
 import com.nbgardens.net_app.entities.User;
+import com.nbgardens.net_app.entities.Wishlist;
 import com.nbgardens.net_app.managers.UserManager;
 import com.nbgardens.net_app.util.InitialData;
 
@@ -13,14 +16,11 @@ public class UserManagerOffline implements UserManager{
 	private InitialData initialData;
 	
 	@Override
-	public void persistUser(User u) {
+	public void persistUser(CardDetails card, Wishlist wishlist, Basket basket, String name, String surname, String email, String phoneNo, String password, Integer permissions, String add1, String add2, String town, String county, String postcode) {
+		User u = new User(card, wishlist, basket, name, surname, email, phoneNo, password, permissions, add1, add2, town, county, postcode);
 		initialData.addUser(u);
 	}
 	
-	@Override
-	public void persistUsers(List<User> u) {
-		
-	}
 	
 	@Override
 	public User getUser(Integer id) {
@@ -35,15 +35,57 @@ public class UserManagerOffline implements UserManager{
 	}
 	@Override
 	public List<User> getUsers() {
-
 		return initialData.getUsers();
 	}
+	
+	
 	@Override
-	public void updateUser(User u) {
+	public void updateUserEmail(Integer id, String email) {
+		int count = 0;
+		List<User> us = initialData.getUsers();
+		for(int i=0; i<us.size(); i++){
+			if(us.get(i).getUserID().equals(id)){
+				us.get(i).setEmail(email);
+				count++;
+			}
+		}
+		if(count==0){
+			System.out.println("No user with that ID found");
+		}
 		
 	}
-	@Override
-	public void removeUser(User u) {
+	
+	
+	public void updateUserPhoneNo(Integer id, String phoneNo) {
+		int count = 0;
+		List<User> us = initialData.getUsers();
+		for(int i=0; i<us.size(); i++){
+			if(us.get(i).getUserID().equals(id)){
+				us.get(i).setPhoneNumber(phoneNo);
+				count++;
+			}
+		}
+		if(count==0){
+			System.out.println("No user with that ID found");
+		}
 		
 	}
+	
+	
+	
+	public void updateUserPassword(Integer id, String password) {
+		int count = 0;
+		List<User> us = initialData.getUsers();
+		for(int i=0; i<us.size(); i++){
+			if(us.get(i).getUserID().equals(id)){
+				us.get(i).setPassword(password);
+				count++;
+			}
+		}
+		if(count==0){
+			System.out.println("No user with that ID found");
+		}
+		
+	}
+	
 }
