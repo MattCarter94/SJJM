b83c05fd923e0cf30cd5d5244e30e69f0c07b64f
joe trost <joseph.trost@qa.com>
2016-10-28 11:17:53 +0100

    updated product to slide correctly

--- nbgardens/src/main/webapp/resources/js/products.js
+++ nbgardens/src/main/webapp/resources/js/products.js
@@ -15,13 +15,8 @@ $(document).ready(function() {
         });
     });
     
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-        acc[i].onclick = function(){
-            this.classList.toggle("active");
-            this.nextElementSibling.classList.toggle("show");
-        }
-    }
+    
+    $('.accordion').click(function(){
+    	$('.panel').slideToggle();
+    });
 });
