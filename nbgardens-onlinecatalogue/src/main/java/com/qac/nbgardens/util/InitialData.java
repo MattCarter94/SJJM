30b7d056d457fb37d80f0799c0efd0bc711f1fdc
stefan-jj <stefanps2@hotmail.com>
2016-11-01 14:35:10 +0000

    fixed website errors

--- nbgardens-onlinecatalogue/src/main/java/com/qac/nbgardens/util/InitialData.java
+++ nbgardens-onlinecatalogue/src/main/java/com/qac/nbgardens/util/InitialData.java
@@ -9,9 +9,12 @@ import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
-
+import com.qac.nbgardens.entities.Basket;
+import com.qac.nbgardens.entities.CardDetails;
+import com.qac.nbgardens.entities.Customer;
 import com.qac.nbgardens.entities.CustomerOrder;
 import com.qac.nbgardens.entities.Product;
+import com.qac.nbgardens.entities.Wishlist;
 import com.qac.nbgardens.entities.enums.Category;
 import com.qac.nbgardens.entities.enums.ProductStatus;
 
@@ -25,10 +28,16 @@ public class InitialData {
 	//private List<StockOrder> stockOrders = new ArrayList<StockOrder>();
 	private List<CustomerOrder> customerOrders = new ArrayList<CustomerOrder>();
 	
+	private List<Customer> users = new ArrayList<Customer>();
+	private List<CardDetails> cards = new ArrayList<CardDetails>();
+	private List<Basket> baskets = new ArrayList<Basket>();
+	
 	
 	@PostConstruct
 	public void SetupData() {
 		//Add users
+		addUser(new Customer(new CardDetails("123456789", "2018-05-03"), new Wishlist(new BigDecimal(20.05)), new Basket(new BigDecimal(20.05)), "Matt", "Carter", "matt@email.co.uk", "02938475812", "password1", 2, "420", "Blaze Rd", "Cranberry", "Oxfordshire", "OX123AB"));
+
 
 		//Add products
 		addProduct(new Product(100, "Green Gnome", new BigDecimal(20.05), "its a gnome", Category.GNOME, "clive.png", "tag1, tag2, tag3", 5000, ProductStatus.ACTIVE, new Date()));
@@ -61,6 +70,19 @@ public class InitialData {
 		return (ArrayList<Product>) products;
 	}
 
+	public List<Customer> getUsers() {
+			return users;
+	}
+	public void addUser(Customer u) {
+	 		users.add(u);
+	}
+	public void addToBasket(Basket b) {
+		 //stub
+	}
 	
+	public List<Basket> getBaskets() {
+		// TODO Auto-generated method stub
+		return baskets;
+	}
 	
 }
