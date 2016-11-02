14cdc9f359dd9c5287e6ee1c03b1578d96bd3f0e
joe trost <joseph.trost@qa.com>
2016-11-02 14:58:50 +0000

    functioning dataset can now be updated

--- nbgardens-ims/src/main/java/com/qac/nbgardens/service/ProductService.java
+++ nbgardens-ims/src/main/java/com/qac/nbgardens/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.qac.nbgardens.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -17,16 +18,32 @@ public class ProductService {
 		return productManager.findAll();
 	}
 	
-	public void updateProduct(Product p) {
-		productManager.findProductById(p.getProductID()).setTitle(p.getTitle());
-		productManager.findProductById(p.getProductID()).setDescription(p.getDescription());
-		productManager.findProductById(p.getProductID()).setImage(p.getImage());
-		productManager.findProductById(p.getProductID()).setPrice(p.getPrice());
-		productManager.findProductById(p.getProductID()).setStock(p.getStock());
-		if (productManager.findProductById(p.getProductID()).getStock() > productManager.findProductById(p.getProductID()).getMaxStock()) {
-			productManager.findProductById(p.getProductID()).setMaxStock(p.getStock());
-		}
-		productManager.findProductById(p.getProductID()).setTags(p.getTags());
+	public void updateProduct(Integer id, String title, String price, String description, String category, String image, String tags, String stock, String active) {
+		System.out.println("Product Updated -: 		ID: " + id + "|| Title: " + title);
+		
+		productManager.findProductById(id).setTitle(title);
+		BigDecimal nPrice = new BigDecimal(Float.parseFloat(price));
+		productManager.findProductById(id).setPrice(nPrice);
+		productManager.findProductById(id).setDescription(description);
+		//TODO productManager.findProductById(id).setCategory()
+		productManager.findProductById(id).setImage(image);
+		productManager.findProductById(id).setTags(tags);
+		productManager.findProductById(id).setStock(Integer.parseInt(stock));
+		//TODO productManager.findProductById(id).setActive()
+		
+		
+//		System.out.println(productManager.findProductById(p.getProductID()).getTitle() + " <- -> " + p.getTitle());
+//		
+//		
+//		productManager.findProductById(p.getProductID()).setTitle(p.getTitle());
+//		productManager.findProductById(p.getProductID()).setDescription(p.getDescription());
+//		productManager.findProductById(p.getProductID()).setImage(p.getImage());
+//		productManager.findProductById(p.getProductID()).setPrice(p.getPrice());
+//		productManager.findProductById(p.getProductID()).setStock(p.getStock());
+//		if (productManager.findProductById(p.getProductID()).getStock() > productManager.findProductById(p.getProductID()).getMaxStock()) {
+//			productManager.findProductById(p.getProductID()).setMaxStock(p.getStock());
+//		}
+//		productManager.findProductById(p.getProductID()).setTags(p.getTags());
 		
 	}
 	
