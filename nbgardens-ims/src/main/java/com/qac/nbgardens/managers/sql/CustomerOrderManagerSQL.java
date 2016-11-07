9fe973a28fe549f8322b3b14c6f7065bd105c4f2
joe trost <joseph.trost@qa.com>
2016-11-07 11:12:20 +0000

    added all sql managers

---
+++ nbgardens-ims/src/main/java/com/qac/nbgardens/managers/sql/CustomerOrderManagerSQL.java
@@ -0,0 +1,10 @@
+package com.qac.nbgardens.managers.sql;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Alternative;
+
+@Alternative
+@Stateless
+public class CustomerOrderManagerSQL {
+
+}
